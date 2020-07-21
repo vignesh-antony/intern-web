6a1049690f844dd993bef3a690729e1454fed39b
unknown <vigneshantony5@gmail.com>
2020-07-21 10:05:47 +0530

    Adding Messaging Folder

---
+++ Messaging/LinkedIn-Chat/JS/main.js
@@ -0,0 +1,20 @@
+var fbar = document.getElementById("filter");
+var ficon = document.getElementById("filter-icon");
+var list = document.getElementsByClassName("opt");
+
+function removeActive(){
+    for(item of list){
+        item.classList.remove("active");
+    }
+}
+
+for(item of list){
+    item.addEventListener("click",function(){
+        removeActive();
+        this.classList.add("active");
+    });
+}
+
+ficon.addEventListener("click",function(){
+    fbar.classList.toggle("hide");
+});
